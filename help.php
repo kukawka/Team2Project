a878a8bed063868ef6f5ac20d41f360f73d4751d
cmckillop <conor303@gmail.com>
2017-09-28 21:30:53 +0100

    Add Functionality to Help Page
    Add viewing of User Guide PDF to help page.

---
+++ help.php
@@ -0,0 +1,13 @@
+<?php
+$pageTitle = "Help";
+$currentPage = "help";
+include "header.php";
+?>
+
+<div class="pdf-container">
+    <object data="assets/DUSAYoyo_Dashboard_Guide.pdf" type="application/pdf" width="100%" height="100%">
+</div>
+
+
+    </body>
+</html>
